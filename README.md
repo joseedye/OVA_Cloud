1661c8fdb13adabcdae21fa6eab20f32dcf07fc4
joseedye <joseeduardorm@ufps.edu.co>
2021-11-27 22:16:11 -0500

    preparandome para backend

---
+++ README.md
@@ -0,0 +1,4 @@
+# OVA_Cloud
+OVA sobre Cloud Computing
+
+Proyecto, creacion de un OVA (objeto virtual de aprendizaje) bajo la tematica del cloud computing.
---
+++ src/test/java/com/educativa/ova/OvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educativa.ova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
